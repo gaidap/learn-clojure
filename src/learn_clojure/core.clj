683ec4e38ffd7c832a08693444a66f6031e0ea52
Paul Gaida <pc.gaida@gmail.com>
2024-10-17 22:46:08 +0200

    Add reduce example and symmetrize functions
    Introduced an example explanation for the `reduce` function. Added `simple-symmetrize-body-parts` to generate symmetric body parts based on provided asymmetric parts. This enhances code readability and functionality for dealing with hobbit body parts.

--- src/learn_clojure/core.clj
+++ src/learn_clojure/core.clj
@@ -1,5 +1,20 @@
 (ns learn-clojure.core)
 
+#_(
+    reduce
+
+    (reduce f coll)(reduce f val coll)
+
+    f should be a function of 2 arguments. If val is not supplied,
+    returns the result of applying f to the first 2 items in coll, then
+    applying f to that result and the third item, etc. If coll contains no
+    items, f must accept no arguments as well, and reduce returns the
+    result of calling f with no arguments.  If coll has only 1 item, it
+    is returned and f is not called.  If val is supplied, returns the
+    result of applying f to val and the first item in coll, then
+    applying f to that result and the second item, etc. If coll contains no
+    items, returns val and f is not called.)
+
 (def asym-hobbit-body-parts [{:name "head" :size 3}
                              {:name "left-eye" :size 1}
                              {:name "left-ear" :size 1}
@@ -36,3 +51,11 @@
         (recur remaining
                (into final-body-parts
                      (set [part (matching-part part)])))))))
+
+(defn simple-symmetrize-body-parts
+  "Expects a seq of mas that have a :name and :size"
+  [asym-body-parts]
+  (reduce (fn [final-body-parts part]
+            (into final-body-parts (set [part (matching-part part)])))
+          []
+          asym-body-parts))
