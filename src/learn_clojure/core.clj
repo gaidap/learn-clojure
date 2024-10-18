e0df5984649f4c582081426bc8c408380caeeac8
Paul Gaida <pc.gaida@gmail.com>
2024-10-18 19:17:10 +0200

    Add new utility functions for printing and collections
    Added `my-function` to print a custom message, `collection-collection` to demonstrate different data structures, and `dec-maker` to create decrement functions. Included commented code for future enhancements.

--- src/learn_clojure/core.clj
+++ src/learn_clojure/core.clj
@@ -5,3 +5,33 @@
   "I don't do a whole lot."
   [& args]
   (println "I'm a little teapot!"))
+
+
+; 1.
+(defn my-function
+  []
+  (println (str "This" " is" " my" " function..."))
+  (println (str "...there" " are" " many" " functions..."))
+  (println (str "...but" " this" " one" " is" " mine!")))
+
+; 2.
+(defn collection-collection
+  []
+  (print "A List: ")
+  (println '(1 2 3 4))
+  (print "A Vetor: ")
+  (println [1 2 3 4])
+  (print "A Hash-Map: ")
+  (println {:key "Nice" :value "One"})
+  (print "A Hash-Set: ")
+  ;This will not work!
+  ;(println #{:a :a :b :c})
+  (println (hash-set :a :a :b :c)))
+
+; 3.
+(defn dec-maker
+  [x]
+  #(- % x))
+(def dec23 (dec-maker 23))
+
+; 4. (mapset inc [1 1 2 2 3]) => #{2 3 4}
