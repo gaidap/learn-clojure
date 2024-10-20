75aa00bc430c26f7419deddf51e20c55279fae5f
Paul Gaida <pc.gaida@gmail.com>
2024-10-20 20:03:18 +0200

    Add vampire identification functions
    Implemented a vampire database and functions to retrieve vampire-related details and identify vampires based on specific traits. This includes checking for blood puns and pulse absence in the records.

--- src/learn_clojure/core.clj
+++ src/learn_clojure/core.clj
@@ -1,6 +1,26 @@
 (ns learn-clojure.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def vampire-database
+  {0 {:makes-blood-puns? false :has-pulse? true :name "McFishwich"}
+   1 {:makes-blood-puns? false :has-pulse? true :name "McMackson"}
+   2 {:makes-blood-puns? true :has-pulse? false :name "Damon Salvatore"}
+   3 {:makes-blood-puns? true :has-pulse? true :name "Mickey Mouse"}
+   4 {:makes-blood-puns? true :has-pulse? false :name "Jay Jamey Jona Jabadou"}
+   })
+
+(defn vampire-related-details
+  [social-security-number]
+  (Thread/sleep 1000)
+  (get vampire-database social-security-number))
+
+(defn vampire?
+  [record]
+  (and (:makes-blood-puns? record)
+       (not (:has-pulse? record))
+       record))
+
+(defn identify-vampire
+  [social-security-numbers]
+  (first (filter vampire?
+                 (map vampire-related-details social-security-numbers))))
+
