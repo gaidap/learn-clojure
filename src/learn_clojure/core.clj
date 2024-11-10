bca4d28b3b280e74d101d6d426c3e6a7aa6dee67
Paul Gaida <pc.gaida@gmail.com>
2024-11-10 22:03:23 +0100

    Add functions for triangular number calculations and connections
    Introduced functions to generate triangular numbers, check if a number is triangular, and retrieve row details based on positions. Also added functionality to form mutual connections between board positions, extending the game's core logic.

--- src/learn_clojure/core.clj
+++ src/learn_clojure/core.clj
@@ -1,6 +1,39 @@
-(ns learn-clojure.core)
+(ns learn-clojure.core
+  (:gen-class))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(declare successful-move prompt-mve game-over query-rows)
+
+(defn tri*
+  "Generates lazy sequence of triangular numbers"
+  ([] (tri* 0 1))
+  ([sum n]
+   (let [new-sum (+ sum n)]
+     (cons new-sum (lazy-seq (tri* new-sum (inc n)))))))
+
+(def tri (tri*))
+
+(defn triangular?
+  "Is the number triangular? e.g. 1,3,6,10,15,..."
+  [n]
+  (= n (last (take-while #(>= n %) tri))))
+
+(defn row-tri
+  "The triangular number at the end of row n"
+  [n]
+  (last (take n tri)))
+
+(defn row-num
+  "Returns row number the position belongs to: pos 1 in row 1,
+  positions 2 and 3 in row 2,..."
+  [pos]
+  (inc (count (take-while #(> pos %) tri))))
+
+(defn connect
+  "Form a mutual connection between two positions"
+  [board max-pos pos neighbour destination]
+  (if (<= destination max-pos)
+    (reduce (fn [new-board [p1 p2]]
+              (assoc-in new-board [p1 :connections p2] neighbour))
+            board
+            [[pos destination] [destination pos]])
+    board))
