3336e9a01122193f8039b529b378e5cc079987f2
Paul Gaida <paul.gaida@dsmz.de>
2024-11-22 13:16:02 +0100

    Add board connection functions and initialize board
    Introduced `connect-right`, `connect-down-left`, and `connect-down-right` functions for establishing connections on the board. Added `add-pos` for pegging positions and performing connections, and `new-board` for creating a board with a specific number of rows.

--- src/learn_clojure/core.clj
+++ src/learn_clojure/core.clj
@@ -37,3 +37,43 @@
             board
             [[pos destination] [destination pos]])
     board))
+
+(defn connect-right
+  [board max-pos pos]
+  (let [neighbour (inc pos)
+        destination (inc neighbour)]
+    (if-not (or (triangular? neighbour) (triangular? pos))
+      (connect board max-pos pos neighbour destination)
+      board)))
+
+(defn connect-down-left
+  [board max-pos pos]
+  (let [row (row-num pos)
+        neighbour (+ row pos)
+        destination (+ 1 row neighbour)]
+    (connect board max-pos pos neighbour destination)))
+
+(defn connect-down-right
+  [board max-pos pos]
+  (let [row (row-num pos)
+        neighbour (+ 1 row pos)
+        destination (+ 2 row neighbour)]
+    (connect board max-pos pos neighbour destination)))
+
+(defn add-pos
+  "Pegs the position and performs connections"
+  [board max-pos pos]
+  (let [pegged-board (assoc-in board [pos :pegged] true)]
+    (reduce (fn [new-board connection-creation-fn]
+              (connection-creation-fn new-board max-pos pos))
+            pegged-board
+            [connect-right connect-down-left, connect-down-right])))
+
+(defn new-board
+  "Creates a new board with the given number of rows"
+  [rows]
+  (let [initial-board {:rows rows}
+        max-pos (row-tri rows)]
+    (reduce (fn [board pos] (add-pos board max-pos pos))
+            initial-board
+            (range 1 (inc max-pos)))))
