0493aa1ec17faf98fdf65cdbb632a292bd9f6e84
Paul Gaida <pc.gaida@gmail.com>
2024-11-25 21:37:52 +0100

    Add functions for triangular number calculations and connections
    Introduced functions to generate triangular numbers, check if a number is triangular, and retrieve row details based on positions. Also added functionality to form mutual connections between board positions, extending the game's core logic.

--- src/learn_clojure/core.clj
+++ src/learn_clojure/core.clj
@@ -1,4 +1,7 @@
 (ns learn-clojure.core
+  (:require [clojure.java.browse :as browse]
+            [learn-clojure.visualization.svg :refer [xml]])
+  (:refer-clojure :exclude [max min])
   (:gen-class))
 
 ; use is equivalent to:
@@ -6,27 +9,45 @@
 ;(refer 'learn-clojure.visualization.svg)
 (use '[learn-clojure.visualization.svg :as svg])
 
-(def heists [{:location "Cologne, Germany"
+(def heists [{:location    "Cologne, Germany"
               :cheese-name "Archbishop Hildebold's Cheese Pretzel"
-              :lat 50.95
-              :lng 6.97}
-             {:location "Zurich, Switzerland"
+              :lat         50.95
+              :lng         6.97}
+             {:location    "Zurich, Switzerland"
               :cheese-name "The Standard Emmental"
-              :lat 47.37
-              :lng 8.55}
-             {:location "Marseille, France"
+              :lat         47.37
+              :lng         8.55}
+             {:location    "Marseille, France"
               :cheese-name "Le Fromage de Cosquer"
-              :lat 43.30
-              :lng 5.37}
-             {:location "Zurich, Switzerland"
+              :lat         43.30
+              :lng         5.37}
+             {:location    "Zurich, Switzerland"
               :cheese-name "The Standard Emmental"
-              :lat 47.37
-              :lng 8.55}
-             {:location "Vatican City"
+              :lat         47.37
+              :lng         8.55}
+             {:location    "Vatican City"
               :cheese-name "The Cheese of Turin"
-              :lat 41.90
-              :lng 12.45}])
+              :lat         41.90
+              :lng         12.45}])
+
+(defn url
+  [filename]
+  (let [dir (System/getProperty "user.dir")]
+    (str "file:///"
+         (.replace dir "\\" "/")
+         "/"
+         filename)))
+
+(defn template
+  [contents]
+  (str "<style>polyline{fill:none;stroke:#5881d8;stroke-width:3}</style>"
+       contents))
 
 (defn -main
   [& args]
-  (println (svg/points heists)))
+  (let [filename "map.html"]
+    (->> heists
+         (xml 50 100)
+         template
+         (spit filename))
+    (browse/browse-url (url filename))))
--- src/learn_clojure/visualization/svg.clj
+++ src/learn_clojure/visualization/svg.clj
@@ -1,4 +1,6 @@
-(ns learn-clojure.visualization.svg)
+(ns learn-clojure.visualization.svg
+  (:refer-clojure :exclude [max min])
+  (:require [clojure.string]))
 
 (defn latAndlng->point
   "Convert latitude / longitude to comma-seperated string"
@@ -9,4 +11,48 @@
   [locations]
   (clojure.string/join "" (map latAndlng->point locations)))
 
+(defn comparator-over-maps
+  [comparison-fn ks]
+  (fn [maps]
+    (zipmap ks
+            (map (fn [k] (apply comparison-fn (map k maps)))
+                 ks))))
 
+(def min (comparator-over-maps clojure.core/min [:lat :lng]))
+(def max (comparator-over-maps clojure.core/max [:lat :lng]))
+
+(defn translate-to-00
+  [locations]
+  (let [mincoords (min locations)]
+    (map #(merge-with - % mincoords) locations)))
+
+(defn scale
+  [width height locations]
+  (let [maxcoords (max locations)
+        ratio {:lat (/ height (:lat maxcoords))
+               :lng (/ width (:lng maxcoords))}]
+    (map #(merge-with * % ratio) locations)))
+
+(defn line
+  [points]
+  (str "<polyline points=\"" points "\"/>"))
+
+(defn transform
+  "Just chains other functions"
+  [width height locations]
+  (->> locations
+       translate-to-00
+       (scale width height)))
+
+(defn xml
+  "scg 'template', which also flips the coordinate system"
+  [width height locations]
+  (str "<svg height=\"" height "\" width=\"" width "\">"
+       ;; These two <g> tags flip the coodinate system
+       "<g transform=\"translate(0," height ")\">"
+       "<g transform=\"scale(1,-1)\">"
+       (-> (transform width height locations)
+           points
+           line)
+       "</g></g>"
+       "</svg>"))
