5226aa4b5e438b06ed91eb8b27e8a4bf229e052b
Paul Gaida <paul.gaida@dsmz.de>
2024-11-29 09:04:13 +0100

    Add code-critic macro to critique code quality
    Replaced the foo function with a new macro, code-critic, which provides humorous feedback on code quality using phrases inspired by Hermes Conrad from Futurama. This macro outputs comments on both 'bad' and 'good' code, enhancing the code review process in a lighthearted manner.

--- src/learn_clojure/core.clj
+++ src/learn_clojure/core.clj
@@ -1,6 +1,9 @@
 (ns learn-clojure.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defmacro code-critic
+  "Phrases are courtesy of Hermes Conrad from Futurama"
+  [bad good]
+  `(do (println "Great squid of Madrid, this is bad code:"
+                (quote ~bad))
+       (println "Sweet gorilla of Manila, this is good code:"
+                (quote ~good))))
