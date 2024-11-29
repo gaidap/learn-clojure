176b1321bee041cb840e6a005207aae2f8a330e9
Paul Gaida <paul.gaida@dsmz.de>
2024-11-29 09:08:27 +0100

    Refactor code-critic macro to use criticize-code function.
    Introduce a new function criticize-code to streamline the code-critic macro by eliminating repetitive println statements. This change enhances code readability and maintainability while preserving the original functionality and humor.

--- src/learn_clojure/core.clj
+++ src/learn_clojure/core.clj
@@ -1,9 +1,11 @@
 (ns learn-clojure.core)
 
+(defn criticize-code
+  [criticism code]
+  `(println ~criticism (quote ~code)))
+
 (defmacro code-critic
   "Phrases are courtesy of Hermes Conrad from Futurama"
   [bad good]
-  `(do (println "Great squid of Madrid, this is bad code:"
-                (quote ~bad))
-       (println "Sweet gorilla of Manila, this is good code:"
-                (quote ~good))))
+  `(do ~(criticize-code "Great squid of Madrid, this is bad code:" bad)
+       ~(criticize-code "Sweet gorilla of Manila, this is good code:" good)))
