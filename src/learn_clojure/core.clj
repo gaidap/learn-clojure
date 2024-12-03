6a4351f3d4ef757340125171958fcd1f42ab2b5e
Paul Gaida <paul.gaida@dsmz.de>
2024-12-03 14:04:53 +0100

    Add distance calculation functions
    Introduced `distance` and `distance-total` functions to compute distances between given locations. These functions support calculating total distances by pairing and sorting location arrays.

--- src/learn_clojure/core.clj
+++ src/learn_clojure/core.clj
@@ -1,6 +1,19 @@
 (ns learn-clojure.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def example-locations [
+                        [3 4 2 1 3 3]
+                        [4 3 5 3 9 3]
+                        ])
+
+(defn distance
+  [loc-a loc-b]
+  (let [d (abs (- loc-a loc-b))]
+    (println loc-a " - " loc-b " = " d)
+    d))
+
+(defn distance-total
+  [left-locs right-locs]
+  (reduce (fn [total-distance loc-pair]
+            (+ total-distance (distance (first loc-pair) (second loc-pair))))
+          0
+          (map vector (sort left-locs) (sort right-locs))))
