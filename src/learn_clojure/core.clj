4be60edb9439e15193d1026e69c54b8e96ab7497
Paul Gaida <pc.gaida@gmail.com>
2024-12-03 19:57:54 +0100

    Add validation logic for order details
    Introduced `order-details` map and corresponding validation rules in `order-details-validations`. Added `error-messages-for` function to generate error messages based on validation failures. This enhances data integrity checks for order processing.

--- src/learn_clojure/core.clj
+++ src/learn_clojure/core.clj
@@ -1,6 +1,18 @@
 (ns learn-clojure.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def order-details
+  {:name  "Mitchard Blimmons"
+   :email "mitchard.blimmonsgmail.com"})
+
+(def order-details-validations
+  {:name
+   ["Please enter a name" not-empty]
+   :email
+   ["Please enter an email address" not-empty
+    "Your email address doesn't look like an email address" #(or (empty? %) (re-seq #"@" %))]})
+
+(defn error-messages-for
+  "Return a seq of error messages"
+  [to-validate message-validator-pairs]
+  (map first (filter #(not ((second %) to-validate))
+                     (partition 2 message-validator-pairs))))
