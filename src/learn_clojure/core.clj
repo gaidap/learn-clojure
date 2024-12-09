cf1b1daa0730fd918b0e8ffc86d16226203972b4
Paul Gaida <paul.gaida@dsmz.de>
2024-12-09 14:58:08 +0100

    Add omni-search with concurrent Google and Bing queries
    This commit introduces a new function, `omni-search`, that performs concurrent search queries using both Google and Bing. The results are handled via promises, delivering the first non-empty response received from either search engine. This enhances the search functionality by providing results faster and increasing reliability.

--- src/learn_clojure/core.clj
+++ src/learn_clojure/core.clj
@@ -1,6 +1,22 @@
 (ns learn-clojure.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+
+(defn google-search
+  [query]
+  (str "Here is your google result for " query "!")
+  (slurp (str  "https://www.google.com/search" "?q=" query)))
+
+
+(defn bing-search
+  [query]
+  (str "Here is your bing result for " query "!")
+  (slurp (str  "https://www.bing.com/search" "?q=" query)))
+
+(defn omni-search
+  [query]
+  (let [search-promise (promise)]
+    (future (if-let [result (not-empty (google-search query))]
+              (deliver search-promise result)))
+    (future (if-let [result (not-empty (bing-search query))]
+              (deliver search-promise result)))
+    @search-promise))
